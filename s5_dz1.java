baffa1cabfa8eaa69157935c241478858c72eb5d
EvgenyaHramova <evgenya.hramova@yandex.ru>
2023-03-25 00:20:28 +0400

    new fail z1.java

--- s5_dz1.java
+++ s5_dz1.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+// НЕ РАБОТАЕТ!!!!!!!!
+
 // Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
 // Добавить функции 1) Добавление номера
 // 2) Вывод всего
@@ -45,48 +47,45 @@ public class s5_dz1 {
     public static Map<String, List<Integer>> new_phone(Map<String, List<Integer>> book_phone) {
         System.out.println("Введите имя: ");
         String name = inputText.nextLine();
-        // 
+        //
 
-                    System.out.printf("%s: %s ", name, book_phone.get(name) + "\n");
-                    System.out.print("Желаете добавить номер в запись: " + name + ", тогда нажмите Y");
-                    String yes = inputText.next().toLowerCase();
-                    if (yes.equals("Y")) {
-                        System.out.println("Введите дополнительный номер телефона: ");
-                        Integer new_num = inputText.nextInt();
-                        for (Map.Entry<String, List<Integer>> item : book_phone.entrySet()) {
-                            if (item.getKey().equals(name)){
-                                List<Integer> val = new ArrayList<>(book_phone.get(name));
-                                val.add(new_num);
-                                book_phone.put(name, val);
-                            }  
-                            else {
-                                System.out.println("Записи с указанными данными нет в списке контактов");
-                            } 
-                        }
-                        System.out.printf("Номера телефонов", name, ": %s ", book_phone.get(name));
-                    }
-                    else {
-                        System.out.println("Ок");
-                    }
-                   
+        System.out.printf("%s: %s ", name, book_phone.get(name) + "\n");
+        System.out.print("Желаете добавить номер в запись: " + name + ", тогда нажмите Y");
+        String yes = inputText.next().toLowerCase();
+        if (yes.equals("Y")) {
+            System.out.println("Введите дополнительный номер телефона: ");
+            Integer new_num = inputText.nextInt();
+            for (Map.Entry<String, List<Integer>> item : book_phone.entrySet()) {
+                if (item.getKey().equals(name)) {
+                    List<Integer> val = new ArrayList<>(book_phone.get(name));
+                    val.add(new_num);
+                    book_phone.put(name, val);
+                } else {
+                    System.out.println("Записи с указанными данными нет в списке контактов");
+                }
+            }
+            System.out.printf("Номера телефонов", name, ": %s ", book_phone.get(name));
+        } else {
+            System.out.println("Ок");
+        }
+
         inputText.close();
         return book_phone;
     }
 
-
     // public static void new_phone(Map<String, List<Integer>> book_phone) {
-    //     System.out.println("Введите имя: ");
-    //     String name = inputText.nextLine();
-    //     if(book_phone.containsKey(name)){
-    //         System.out.println("Введите дополнительный номер телефона: ");
-    //         Integer new_num = inputText.nextInt();
-    //         book_phone.get(name).add(new_num);
-    //         System.out.printf("Номера телефонов", name, ": %s ", book_phone.get(name));
-    //     }
+    // System.out.println("Введите имя: ");
+    // String name = inputText.nextLine();
+    // if(book_phone.containsKey(name)){
+    // System.out.println("Введите дополнительный номер телефона: ");
+    // Integer new_num = inputText.nextInt();
+    // book_phone.get(name).add(new_num);
+    // System.out.printf("Номера телефонов", name, ": %s ", book_phone.get(name));
+    // }
     // }
 
     public static void main(String[] args) {
         b_ph();
-        new_phone(book_phone);        
-    }       
+        new_phone(book_phone);
+    }
 }
